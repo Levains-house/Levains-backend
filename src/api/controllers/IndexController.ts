b87365472344b8970c4f916b1fa84a9567887419
CokeLee777 <akak640@kyonggi.ac.kr>
2022-12-19 18:42:17 +0900

    delete trash logic

--- src/api/controllers/IndexController.ts
+++
@@ -1,8 +0,0 @@
-import {Request, Response} from "express";
-import router from "./UsersController";
-
-router.get("/api/health-check", async (request: Request, response: Response) => {
-    return response.sendStatus(200);
-});
-
-export default router;
